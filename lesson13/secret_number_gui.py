05488a787c58a0fb38b060dcd45e60bf0ee57e77
Matej Ramuta <matej.ramuta@gmail.com>
2017-01-23 10:38:54 +0100

    secret num gui

---
+++ lesson13/secret_number_gui.py
@@ -0,0 +1,33 @@
+import Tkinter
+import tkMessageBox
+import random
+
+secret = random.randint(1, 100)
+
+window = Tkinter.Tk()
+
+# greeting text
+greeting = Tkinter.Label(window, text="Guess the secret number!")
+greeting.pack()
+
+# guess entry field
+guess = Tkinter.Entry(window)
+guess.pack()
+
+
+# check guess
+def check_guess():
+    if int(guess.get()) == secret:
+        result_text = "CORRECT!"
+    elif int(guess.get()) > secret:
+        result_text = "WRONG! Your guess is too high."
+    else:
+        result_text = "WRONG! Your guess is too low."
+
+    tkMessageBox.showinfo("Result", result_text)
+
+# submit button
+submit = Tkinter.Button(window, text="Submit", command=check_guess)  # check_guess, not check_guess()
+submit.pack()
+
+window.mainloop()
