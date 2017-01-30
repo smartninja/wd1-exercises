416db2efc4b4e282f3a0a8d4396057f9471e787c
Matej Ramuta <matej.ramuta@gmail.com>
2017-01-30 14:49:51 +0100

    current time app

--- lesson14/my-first-project/README.md
+++ lesson14/my-first-project/README.md
@@ -7,4 +7,4 @@ Basic boilerplate code for Google App Engine projects.
 1. Fork this project
 1. Clone the forked repository (on your profile)
 2. Run the code with Google App Engine Launcher (File/Add existing application) or with this command: `python dev_appserver.py ./`
-3. In Launcher click on the app and then Run and Browse. If you ran the app from the command line, open your browser and go to: `http://localhost:8080/`
+3. In Launcher click on the app and then Run and Browse. If you ran the app from the command line, open your browser and go to: `http://localhost:8080/fakebook.html`
---
+++ lesson15/current-time-app/main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import os
+
+import datetime
+import jinja2
+import webapp2
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
+
+
+class BaseHandler(webapp2.RequestHandler):
+
+    def write(self, *a, **kw):
+        return self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        return self.write(self.render_str(template, **kw))
+
+    def render_template(self, view_filename, params=None):
+        if not params:
+            params = {}
+        template = jinja_env.get_template(view_filename)
+        return self.response.out.write(template.render(params))
+
+
+class MainHandler(BaseHandler):
+    def get(self):
+        curr_time = datetime.datetime.now()
+
+        params = {"curr_time": curr_time}
+
+        return self.render_template("hello.html", params=params)
+
+app = webapp2.WSGIApplication([
+    webapp2.Route('/', MainHandler),
+], debug=True)
